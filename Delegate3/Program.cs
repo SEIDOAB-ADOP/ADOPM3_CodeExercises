56046332442b68f5e941ac79391fe2a73e020b36
Martin <martin_lenart@icloud.com>
2024-12-03 12:33:31 +0100

    update

--- Delegate3/Program.cs
+++ Delegate3/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security;
 using Delegate3;
 
 Console.WriteLine("Hello, Friends!");
 
 var friends = new FriendList(10);
 System.Console.WriteLine(friends);
+
+
 
