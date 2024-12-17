3f16b04c5f861109257cb8d82ad637df228aadc5
Martin <martin_lenart@icloud.com>
2024-12-17 11:30:50 +0100

    update

--- ThreadSafeData/Program.cs
+++ ThreadSafeData/Program.cs
@@ -15,41 +15,32 @@ namespace ThreadSafeData
         //Thread Safe Datastructure
         public class VehicleStorage
         {
-            object _locker = new object();
             List<Vehicle> _vehicles = new List<Vehicle>();
 
             public void SetData (string regNr, string owner)
             {
-                lock (_locker)
-                {
-                    var v = new Vehicle() { RegistrationNumber = regNr, Owner = owner };
-                    _vehicles.Add(v);
-                }
+                var v = new Vehicle() { RegistrationNumber = regNr, Owner = owner };
+                _vehicles.Add(v);
             }
 
             public Vehicle GetData (int idx)
             {
-                lock(_locker)
-                {
-                    return _vehicles[idx];
-                }
+                return _vehicles[idx];
             }
 
             public bool CheckConsistency()
             {
-                lock (_locker)
+                foreach (var v in _vehicles)
                 {
-                    foreach (var v in _vehicles)
+                    if ((v.RegistrationNumber, v.Owner) != ("ABC 123", "Kalle Anka") &&
+                        (v.RegistrationNumber, v.Owner) != ("HKL 556", "Musse Pigg"))
                     {
-                        //Verify data consistency - give error if not consistent
-                        if ((v.RegistrationNumber, v.Owner) != ("ABC 123", "Kalle Anka") &&
-                            (v.RegistrationNumber, v.Owner) != ("HKL 556", "Musse Pigg"))
-                        {
-                            return false;
-                        }
+                        //Verify data consistency - write error if not consistent
+                        System.Console.WriteLine($"RegNr: {v.RegistrationNumber}, Owner: {v.Owner}");
+                        return false;
                     }
-                    return true;
                 }
+                return true;
             }
         }
 
@@ -64,14 +55,13 @@ namespace ThreadSafeData
                 for (int i = 0; i < 1000; i++)
                 {
                     //Write Data to Vehicle, "ABC 123", "Kalle Anka"
-                    myCar.SetData("ABC 123", "Kalle Anka");
 
-                    //introduce some system delay
+                    //introduce some random system delay between 1 and 5 milliseconds
                     Task.Delay(rnd.Next(1, 5)).Wait();
 
                     //Read Data from Vehicle
-                    var t = myCar.GetData(i);
 
+                    //Check data consistency
                     if (!myCar.CheckConsistency())
                     {
                         Console.WriteLine("Data inconsistent!");
@@ -86,19 +76,12 @@ namespace ThreadSafeData
                 for (int i = 0; i < 1000; i++)
                 {
                     //Write Data to Vehicle, "HKL 556", "Musse Pigg"
-                    myCar.SetData("HKL 556", "Musse Pigg");
 
-                    //introduce some system delay
-                    Task.Delay(rnd.Next(1, 5)).Wait();
+                    //introduce some random system delay between 1 and 5 milliseconds
 
                     //Read Data from Vehicle
-                    var t = myCar.GetData(i);
 
                     //Verify data consistency - give error if not consistent
-                    if (!myCar.CheckConsistency())
-                    {
-                        Console.WriteLine("Data inconsistent!");
-                    }
                 }
                 Console.WriteLine("t2 Finished");
             });
@@ -111,9 +94,13 @@ namespace ThreadSafeData
     }
 }
 /*  Exercise
-    1. Make class Vehicle Thread safe using lock(...)
-    2.  - Have task t1 write 1000 times "ABC 123", "Kalle Anka" to myCar
+    1.  - Have task t1 write 1000 times "ABC 123", "Kalle Anka" to myCar
         - Have task t2 write 1000 times "HKL 556", "Musse Pigg" to myCar
         - Verify data consistency
         - Discuss in the group what is data consistency in case of class Vehicle. Is your code living up to it?
+
+    2. Make class Vehicle Thread safe using lock(...)
+        - Verify data consistency
+        - Discuss in the group what is data consistency in case of class Vehicle. Is your code living up to it?
+
 */
