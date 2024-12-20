b681828058ab56a99f577b4adf85e8ce57fdb71e
Martin <martin_lenart@icloud.com>
2024-12-20 16:48:46 +0100

    update

--- ThreadSafeData/Program.cs
+++ ThreadSafeData/Program.cs
@@ -4,73 +4,83 @@ using System.Threading.Tasks;
 
 namespace ThreadSafeData
 {
-    class Program
+    internal class Program
     {
-
-        //Thread Safe Datastructure
-        public class VehicleStorage
+        public class Vehicle
         {
-            private readonly Random _rnd = new Random();
-            private readonly List<Vehicle> _vehicles = new List<Vehicle>();
+            public string Owner  {get; set;}
+            public string CarBrand  {get; set;}
+            public string RegNr {get; set;}
+        }
+        
+        public class VechicleStorage
+        {
+            string Owner;
+            string CarBrand;
+            string RegNr;
 
-            public void SetData (string regNr, string owner)
+            Random rnd = new Random();
+            Vehicle _vehicle;
+            List<Vehicle> _vehicles = new List<Vehicle>();
+
+            public int Count()
             {
-                //Do something with regNr and owner before writing to the list
-                string anotherRegNr = regNr;
-                string anotherOwner = owner;
-                Task.Delay(_rnd.Next(1, 5)).Wait();
-
-                if ((anotherRegNr, anotherOwner) != ("ABC 123", "Kalle Anka") &&
-                    (anotherRegNr, anotherOwner) != ("HKL 556", "Musse Pigg"))
-                       System.Console.WriteLine($"RegNr: {anotherRegNr}, Owner: {anotherOwner}");
-
-
-                var v = new Vehicle() {RegistrationNumber = anotherRegNr, Owner = anotherOwner};
-                _vehicles.Add(v);
+                return _vehicles.Count;
             }
-
-            public Vehicle GetData (int idx)
+            public void SetData(string owner, string carBrand, string regNr)
             {
-                return _vehicles[idx];
+                //I somehow manipulate the data
+                Owner = owner;
+                CarBrand = carBrand;
+                Thread.Sleep(rnd.Next(1, 5));
+                RegNr = regNr;
+
+                //Finally, create the instance
+                _vehicle = new Vehicle(){ Owner = Owner,  CarBrand = CarBrand, RegNr = RegNr};
+                
+                //Place it into the list
+                _vehicles.Add(_vehicle);
+
+                //Check data consistency
+                // if ((_vehicle.Owner, _vehicle.CarBrand, _vehicle.RegNr) != ("Donald Duck", "Volvo", "JKF 345") &&
+                //     (_vehicle.Owner, _vehicle.CarBrand, _vehicle.RegNr) != ("Mickey Mouse", "Jaguar", "EFD 235"))
+                //     Console.WriteLine($"mySafe mismatch: Owner:{_vehicle.Owner}, CarBrand:{_vehicle.CarBrand}, RegNr:{_vehicle.RegNr}");              
+            }
+            public (string, string, string) GetData()
+            {
+                return (_vehicle.Owner, _vehicle.CarBrand, _vehicle.RegNr); 
             }
 
             public bool CheckConsistency()
             {
                 foreach (var v in _vehicles)
                 {
-                    if ((v.RegistrationNumber, v.Owner) != ("ABC 123", "Kalle Anka") &&
-                        (v.RegistrationNumber, v.Owner) != ("HKL 556", "Musse Pigg"))
+                    if ((v.Owner, v.CarBrand, v.RegNr) != ("Donald Duck", "Volvo", "JKF 345") &&
+                        (v.Owner, v.CarBrand, v.RegNr) != ("Mickey Mouse", "Jaguar", "EFD 235"))
                     {
-                        //Verify data consistency - write error if not consistent
-                        System.Console.WriteLine($"RegNr: {v.RegistrationNumber}, Owner: {v.Owner}");
+                        Console.WriteLine($"mySafe mismatch: Owner:{v.Owner}, CarBrand:{v.CarBrand}, RegNr:{v.RegNr}");
                         return false;
                     }
                 }
                 return true;
-            }
-            
-            public class Vehicle
-            {
-                public string RegistrationNumber { get; set; }
-                public string Owner { get; set; }
-            }
+            }            
         }
 
         static void Main(string[] args)
         {
-            var myCar = new VehicleStorage();
-            var rnd = new Random();
+            var vechicleStorage = new VechicleStorage();
 
             var t1 = Task.Run(() =>
             {
                 var rnd = new Random();
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < 1_000; i++)
                 {
-                    //Write Data to Vehicle, "ABC 123", "Kalle Anka"
-                    myCar.SetData("ABC 123", "Kalle Anka");
-                    Task.Delay(rnd.Next(1, 5)).Wait();
-                    
-                    myCar.GetData(i);
+                    vechicleStorage.SetData("Mickey Mouse", "Jaguar", "EFD 235");
+
+                    //(string owner, string carBrand, string regNr) = vechicleStorage.GetData();
+                    // if ((owner, carBrand, regNr) != ("Donald Duck", "Volvo", "JKF 345") &&
+                    //     (owner, carBrand, regNr) != ("Mickey Mouse", "Jaguar", "EFD 235"))
+                    //     Console.WriteLine($"mySafe mismatch: Owner:{owner}, CarBrand:{carBrand}, RegNr:{regNr}");
                 }
                 Console.WriteLine("t1 Finished");
             });
@@ -78,13 +88,14 @@ namespace ThreadSafeData
             var t2 = Task.Run(() =>
             {
                 var rnd = new Random();
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < 1_000; i++)
                 {
-                    //Write Data to Vehicle, "HKL 556", "Musse Pigg"
-                    myCar.SetData("HKL 556", "Musse Pigg");
-                    Task.Delay(rnd.Next(1, 5)).Wait();
-                    
-                    myCar.GetData(i);
+                    vechicleStorage.SetData("Donald Duck", "Volvo", "JKF 345");
+
+                    //(string owner, string carBrand, string regNr) = vechicleStorage.GetData();
+                    // if ((owner, carBrand, regNr) != ("Donald Duck", "Volvo", "JKF 345") &&
+                    //     (owner, carBrand, regNr) != ("Mickey Mouse", "Jaguar", "EFD 235"))
+                    //     Console.WriteLine($"mySafe mismatch: Owner:{owner}, CarBrand:{carBrand}, RegNr:{regNr}");
                 }
                 Console.WriteLine("t2 Finished");
             });
@@ -92,22 +103,26 @@ namespace ThreadSafeData
             Task.WaitAll(t1, t2);
             Console.WriteLine("All Finished");
 
-            //Check data consistency
-            if (!myCar.CheckConsistency())
-            {
-                Console.WriteLine("Data inconsistent!");
-            }
+            System.Console.WriteLine(vechicleStorage.Count());
+            System.Console.WriteLine(vechicleStorage.CheckConsistency());
         }
-    }
+    }  
 }
 /*  Exercise
-    1.  - Have task t1 write 1000 times "ABC 123", "Kalle Anka" to myCar
-        - Have task t2 write 1000 times "HKL 556", "Musse Pigg" to myCar
+    1.  - Have task t1 write 1000 times "Mickey Mouse", "Jaguar", "EFD 235" to vechicleStorage.SetData
+        - Have task t2 write 1000 times "Donald Duck", "Volvo", "JKF 345" to vechicleStorage.SetData
         - Verify data consistency
-        - Discuss in the group what is data consistency in case of class Vehicle. Is your code living up to it?
+        - Discuss in the group what is data consistency means here and why is it wrong
+    
+    2.  - in both tasks, use vechicleStorage.GetData() to read the data right after writing it with vechicleStorage.SetData
+        - The data read could be either of below.  Both are correct. Do you understand why?
+             (owner, carBrand, regNr) == ("Donald Duck", "Volvo", "JKF 345") or
+             (owner, carBrand, regNr) == ("Mickey Mouse", "Jaguar", "EFD 235")
+        - in both tasks, write an if statement after vechicleStorage.GetData() to test data consistency. Write an error if not consistent
 
-    2. Make class Vehicle Thread safe using lock(...)
-        - Verify data consistency
-        - Discuss in the group what is data consistency in case of class Vehicle. Is your code living up to it?
+    3. Make class Vehicle Thread safe using lock(...)
+        - Verify data consistency is now correct
 
+    4. Add another tasks that writes 100 times "Scrooge McDuck", "Lamborghini", "SCROOGE" to vechicleStorage.SetData
+        - Verify data consistency is still correct
 */
