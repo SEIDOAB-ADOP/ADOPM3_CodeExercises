9a4e63ada60d3564684dc355180fbe041dca268e
Martin <martin_lenart@icloud.com>
2025-12-17 07:43:19 +0100

    update

---
+++ Enumerable1/Employees/Employee.cs
@@ -0,0 +1,11 @@
+using Models.Employees.Interfaces;
+using Newtonsoft.Json;
+using Seido.Utilities.SeedGenerator;
+
+namespace Models.Employees;
+
+// Implement the IEmployee interface here
+// public class Employee:IEmployee, ISeed<Employee>
+// {
+    
+// }
